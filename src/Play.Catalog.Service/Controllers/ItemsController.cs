0b75ca117a628d27ea399548ef8c8d45bf867b6e
ryanguyen206 <ryanninja595@gmail.com>
2023-08-16 13:38:16 -0700

    Create ItemRepository for abstraction

--- src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -23,10 +23,57 @@ namespace Play.Catalog.Service.Controllers{
         }
 
         [HttpGet("{id}")]
-
-        public ItemDto GetById(Guid id) {
+        public ActionResult<ItemDto> GetById(Guid id) {
             var item = items.Where(item => item.Id == id).SingleOrDefault();
+            if (item == null) {
+                return NotFound();
+            }
             return item;
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> Post(CreateItemDto createItemDto) 
+        {
+            bool itemIsAlreadyCreated = items.Exists(item => item.Name == createItemDto.Name);
+
+            if (itemIsAlreadyCreated) {
+                return BadRequest("An item with the same name already exists.");
+            }
+            var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
+            items.Add(item);
+
+            return CreatedAtAction(nameof(GetById), new {id= item.Id}, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, UpdateItemDto updatedItemDto) {
+            var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
+            
+            if (existingItem == null) {
+                return NotFound();
+            }
+            var updatedItem = existingItem with{
+                Name = updatedItemDto.Name,
+                Description = updatedItemDto.Description,
+                Price = updatedItemDto.Price
+            };
+
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+            items[index] = updatedItem;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id) 
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+            if (index == -1 ) {
+                return NotFound();
+            }
+            items.RemoveAt(index);
+
+            return NoContent();
         }
     }
 }
